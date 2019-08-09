5b245cfcd467914fc13e41f05b44b2a52aa77add
sandeepmareedu007 <53848652+sandeepmareedu007@users.noreply.github.com>
2019-08-09 15:59:44 +0530

    Add files via upload

---
+++ averageprime.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? j;
+   ?? n;
+   ?? c;
+   ?? i;
+   ?? sum;
+
+   i =1;
+   j =1;
+   c =0;
+   sum =0;
+   raptor_prompt_variable_zzyz ="enter range";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> n;
+   while (1)
+   {
+      j =1;
+      if (i>n)) break;
+      while (!(j>i))
+      {
+         if ((i % j)==0)
+         {
+            c =c+1;
+         }
+         else
+         {
+         }
+         j =j+1;
+      }
+      if (c==2)
+      {
+         cout << i << endl;         sum =sum+i;
+      }
+      else
+      {
+      }
+      c =0;
+      i =i+1;
+   }
+   cout << "sum of prime numbers"+sum << endl;   cout << "average of prime numbers"+sum/n << endl;
+   return 0;
+}
